e9a6eb1f1eb8c5af56da9c2f0df2be1dfa24ded9
GunGupta0601 <gungupta0601@gmail.com>
2026-06-04 17:33:19 +0530

    Created Navbar, Hero Section and Latest Collection

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Collection from "./Pages/Collection";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import Login from "./Pages/Login";
+import PlaceOrder from "./Pages/PlaceOrder";
+import Orders from "./Pages/Orders";
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Collection" element={<Collection />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Product/:productId" element={<Product />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/PlaceOrder" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
