eded050b907f73e5b5dbd4ca95f33e9bc583c10b
GunGupta0601 <gungupta0601@gmail.com>
2026-06-14 23:16:06 +0530

    Created Route for Admin Login

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -67,9 +67,28 @@ const listProduct = async (req, res) => {
 };
 
 // function for removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try{
+    await productModel.findByIdAndDelete(req.body.id)
+    res.json({success: true, message: "Product removed"})
+  }
+  catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // function for single product
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try{
+    const {productId}=req.body
+    const product = await productModel.findById(productId)
+    res.json({success: true, product})
+  }
+  catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
